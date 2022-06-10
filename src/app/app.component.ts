0058450000c92d9125cd4cb7245b22bc031a403a
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-06-10 09:55:12 +0300

    Angular Data Binding Learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Angular-Data-Binding';
+  course = 'Angular Course';
+  imageUrl = '../assets/Logo.svg';
+  count = 0;
+  name = '';
+
+  goBack(): void {
+    this.count = this.count + 1;
+    console.log(`Go Back Clicked ${this.count} times!`);
+  }
 }
